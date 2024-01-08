cd14664239e9f433957fe892cbbc1572fca77f0b
HelloWord <chhunlychorn08@gmail.com>
2024-01-08 22:38:12 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name=sc.next();
+        System.out.print("Name is ");
+        System.out.print(name);
+    }
+}
